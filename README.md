b560872e0b763d6b137782a2b10ce1d6b940f0e8
Rafael Victor <rafaelvxl@gmail.com>
2021-10-19 22:59:46 -0300

    c1 - Primeiro commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Sistema de compra de pizzas
+### Curso Fullstack - B7Web
+<br/>
+<div>
+    <img src="assets/to_readme/pc.gif">
+<div><br>
+
+<div>
+    <img width="200" src="assets/to_readme/mobile.gif">
+<div>
+<br/>
+
+### Implementações:
+
+- Preços variados;
+- Mobile (carrinho com valor total de pizzas).
+
+<br/>
+
+### Tecnologias utilizadas:
+
+- HTML5;
+- CSS3;
+- JAVASCRIPT.
---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:[10.5, 15.7, 20.19], sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[10.2, 15.4, 18.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[13.5, 14.7, 17.45], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:[11.5, 17.7, 19.77], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:[16.5, 19.7, 21.43], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[10.4, 15.8, 18.55], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:[10.6, 15.9, 22.36], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
